ae69c7bd5b80feba682c554054b90bee8eb0543e
Diego Cali <diegocali123@gmail.com>
2024-12-06 17:53:50 -0600

    added: customer & orderdetail model & colors for routes

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,14 +1,14 @@
 require('dotenv').config();
 import express from 'express';
-import { Order } from './models/OrderModel';
+import { OrderDetail } from './models/OrderDetailModel';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  console.log('GET /');
-    Order.findAll().then((orders) => {            
-        res.send(orders);
+  console.log('\x1b[36m%s\x1b[0m', 'GET /');
+    OrderDetail.findAll().then((orderdetails) => {            
+        res.send(orderdetails);
     });
 });
 
---
+++ Backend/src/models/CustomerModel.ts
@@ -0,0 +1,36 @@
+import { sql } from "./Squelize";
+import { DataTypes } from "@sequelize/core";
+
+export const Customer = sql.define(
+    "Customer",
+    {
+        CustomerId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        company_name: {
+            type: DataTypes.STRING(45),
+            allowNull: true,
+        },
+        comercial_name: {
+            type: DataTypes.STRING(45),
+            allowNull: true,
+        },
+        delivery_address: {
+            type: DataTypes.STRING(45),
+            allowNull: true,
+        },
+        phone: {
+            type: DataTypes.STRING(45),
+            allowNull: true,
+        },
+        email: {
+            type: DataTypes.STRING(45),
+            allowNull: true,
+        },
+    },
+    {
+        timestamps: false,
+    }
+);
